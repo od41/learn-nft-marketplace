577dfe1fad16108d94b49767c07da2ab26b054b5
Odafe Aror <odafe41@gmail.com>
2022-04-21 19:20:41 +0000

    minting & view mints

--- nft-contract/src/enumeration.rs
+++ nft-contract/src/enumeration.rs
@@ -26,7 +26,20 @@ impl Contract {
         account_id: AccountId,
         from_index: Option<U128>,
         limit: Option<u64>,
-    ) {
-        // do stuff
+    ) -> Vec<JsonToken> {
+        let tokens_for_owner_set = self.tokens_per_owner.get(&account_id);
+        let tokens = if let Some(tokens_for_owner_set) = tokens_for_owner_set {
+            tokens_for_owner_set
+        } else {
+            return vec![];
+        };
+
+        let start = u128::from(from_index.unwrap_or(U128(0)));
+
+        tokens.iter()
+            .skip(start as usize)
+            .take(limit.unwrap_or(50) as usize)
+            .map(|token_id| self.nft_token(token_id.clone()).unwrap())
+            .collect()
     }
 }
